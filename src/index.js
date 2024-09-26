d13fe6c7a93cff6688e8414c596caa82a5e1f0d4
Jitendra Choudhary <jsirvibali@gamil.com>
2024-09-27 00:02:34 +0530

    configered

--- src/index.js
+++ src/index.js
@@ -1,35 +1,18 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
-import express from "express"
 dotenv.config({
-    path: './env'
-})
-const app = express()
+  path: './env'
+});
 
-;(async () => {
-    try {
-        // Connect to the database
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("ERROR: ", error);
-            throw error
-        })
-        app.listen(process.env.PORT, () => {
-           console.log("APP is listening") 
-        })
-    }
-    catch (error) {
-        console.error(error);
-        throw error
-    }
-})
-()
+connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 3000)
-    console.log(`server is running at port : ${process.env.PORT}`)
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`
+    )
 })
-.catch((err) =>{
-    console.log("Connection is failed", err);
-}) 
+})
+.catch((err) => {
+    console.error(err.message);
+    });
